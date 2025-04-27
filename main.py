3d1570043a688db29748694cf15f8a2a358aa6d7
Riandra4trend <18221077@std.stei.itb.ac.id>
2025-04-27 14:56:00 +0700

    output video

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     video_frames = read_video('input_videos/08fd33_4.mp4')
 
     # Initialize Tracker
-    tracker = Tracker('models/best1.pt')
+    tracker = Tracker('models/best.pt')
 
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
